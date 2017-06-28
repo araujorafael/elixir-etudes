31756f8aa88f63d24ab441ae3bb51cf1490b01ea
Rafael Araujo dos Santos <rafael.a.asantos@hsm.com.br>
2017-06-28 09:42:26 -0300

    Étude 6-2: Accumulating the Sum of a List

--- dates.ex
+++ dates.ex
@@ -1,11 +1,11 @@
 defmodule Dates do
     @moduledoc """
-        Étude 5-3: Using String.split
-        Write a module named Dates that contains a function date_parts/1,
-        which takes a string in ISO date format ("yyyy-mm-dd") and returns
-        a list of integers in the form [yyyy, mm, dd].
-        This function does not need to do any error checking.
+        Functions for manipulating calendar dates.
+
+        from *Études for Elixir*, O'Reilly Media, Inc., 2013.
+        Copyright 2013 by J. David Eisenberg.
     """
+    @vsn 0.3
 
     @doc """
         parse a given string date on format yyyy-mm-dd and return a list with
@@ -19,5 +19,45 @@ defmodule Dates do
            String.to_integer(str_m),
            String.to_integer(str_d)
         ]
+    end
+
+    @doc """
+        Étude 6-2
+        Return de sum of all the days has passed to given date on string format "yyyy-mm-dd"
+    """
+
+    @spec julian(Strng.t()) :: number()
+
+    def julian(date) do
+        #transforma date in a list
+        [year, month, day] = date_parts(date)
+        daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        total = month_total(month, daysPerMonth, 0) + day
+
+        cond do
+            is_leap_year(year) && month > 2 -> total + 1
+            true -> total
+        end
+
+    end
+
+    # Recursive funtion that calculates the sum
+
+    defp month_total(1, _daysPerMonth, total) do
+        total
+    end
+
+    @spec month_total(number(), list([number()]), number()) :: number()
+
+    defp month_total(monthNumber, [monthDays|daysPerMonth], total) do
+        month_total(monthNumber - 1, daysPerMonth, total + monthDays)
+    end
+
+    # Verify if given year is leap year
+
+    defp is_leap_year(year) do
+      (rem(year,4) == 0 and rem(year,100) != 0)
+      or (rem(year, 400) == 0)
     end
 end
